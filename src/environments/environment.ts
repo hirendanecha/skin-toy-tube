9e7d5be98d39f6586cddf8ee475faec7ef7fc510
vatsal <vso.opash@gmail.com>
2024-05-01 15:00:22 +0530

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+const loginUrl = 'https://organic.dating/login';
+const logoutUrl = 'https://organic.dating/logout';
+const frontendUrl = 'https://tube.organic.dating/';
+const backendUrl =  'https://api.organic.dating/';
+
+// const frontendUrl = 'http://localhost:4200/';
+// const backendUrl = 'http://localhost:8080/';
+// const loginUrl = 'http://localhost:4200/login';
+const wasabiUrl = 'https://organic-dating.s3.wasabisys.com/'
+
+
+export const environment = {
+  production: false,
+  frontendUrl: frontendUrl,
+  backendUrl: backendUrl,
+  loginUrl: loginUrl,
+  apiUrl: `${backendUrl}api/v1/`,
+  domain: '.organic.dating',
+  wasabiUrl: wasabiUrl,
+  socketUrl: `${backendUrl}`,
+  conferenceUrl: 'https://facetime.tube/',
+  logoutUrl: logoutUrl
+
+};
+
